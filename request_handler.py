841376b05fe7ea04cfbeca65f550ed86abff3288
Jacob Eckert <jweckert17@gmail.com>
2020-10-07 14:11:47 -0500

    updating all indentation to 4-space

--- request_handler.py
+++ request_handler.py
@@ -17,62 +17,62 @@ HANDLERS = {
 class HandleRequests(BaseHTTPRequestHandler):
 
   # A class function
-  def _set_headers(self, status):
-    self.send_response(status)
-    self.send_header('Content-type', 'application/json')
-    self.send_header('Access-Control-Allow-Origin', '*')
-    self.end_headers()
+    def _set_headers(self, status):
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
 
-  def parse_url(self, path):
-    path_params = path.split("/")
-    resource = path_params[1]
-    id = None
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+        id = None
 
-    try:
-      id = int(path_params[2])
-    except IndexError:
-      pass
-    except ValueError:
-      pass
+        try:
+            id = int(path_params[2])
+        except IndexError:
+            pass
+        except ValueError:
+            pass
 
-    return (resource, id) # This is called a "tuple"
+        return (resource, id) # This is called a "tuple"
 
   # A method on the class overriding parent method, handles GET requests
-  def do_GET(self):
-    print(self.path)
+    def do_GET(self):
+        print(self.path)
 
-    self._set_headers(200)
-    response = {} # default response
+        self._set_headers(200)
+        response = {} # default response
 
-    (resource, id) = self.parse_url(self.path)
+        (resource, id) = self.parse_url(self.path)
 
-    # get dictionary of specific request handlers for this resource
-    resource_handlers = HANDLERS[resource]
-    
-    if(id is not None):
-      response = f"{resource_handlers['get_single'](id)}"
-    else:
-      response = f"{resource_handlers['get_all']()}"
+        # get dictionary of specific request handlers for this resource
+        resource_handlers = HANDLERS[resource]
+        
+        if(id is not None):
+            response = f"{resource_handlers['get_single'](id)}"
+        else:
+            response = f"{resource_handlers['get_all']()}"
 
-    self.wfile.write(f"{response}".encode())
+        self.wfile.write(f"{response}".encode())
 
   # Another overriding method that handles POST requests
-  def do_POST(self):
-    self._set_headers(201) # 201 - CREATED
+    def do_POST(self):
+        self._set_headers(201) # 201 - CREATED
 
-    content_len = int(self.headers.get('content-length', 0))
-    post_body = self.rfile.read(content_len)
-    response = f"received post request:<br>{post_body}"
-    self.wfile.write(response.encode())
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        response = f"received post request:<br>{post_body}"
+        self.wfile.write(response.encode())
 
-  # Yet another overriding method that handles PUT requests
-  def do_PUT(self):
-    self.do_POST() # nailed it
+    # Yet another overriding method that handles PUT requests
+    def do_PUT(self):
+        self.do_POST() # nailed it
 
 def main():
-  host = ''
-  port = 8088
-  HTTPServer((host, port), HandleRequests).serve_forever()
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
 
 if __name__ == "__main__":
-  main()
+    main()
