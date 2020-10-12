a7b99b1f7d26ad00df0df6ed533e09af1bc71d80
Jacob Eckert <jweckert17@gmail.com>
2020-10-12 11:01:18 -0500

    implementing get all and get single queries and handlers for locations

--- locations/request.py
+++ locations/request.py
@@ -1,3 +1,7 @@
+import sqlite3
+import json
+
+from models import Location
 from helpers import get_next_id, remove_item_by_id, replace_item_with_matching_id
 
 LOCATIONS = [
@@ -14,14 +18,43 @@ LOCATIONS = [
 ]
 
 def get_all_locations():
-    return LOCATIONS
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            l.id,
+            l.name,
+            l.address
+        FROM Location l
+        """)
+
+        results = db_cursor.fetchall()
+
+        locations = [ (Location(**location)).__dict__ for location in results ]
+
+    return json.dumps(locations)
 
 def get_single_location(id):
-    for location in LOCATIONS:
-        if(location["id"] == id):
-            return location
-    
-    return None
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            l.id,
+            l.name,
+            l.address
+        FROM Location l
+        WHERE l.id = ?
+        """, ( id, ))
+
+        result = db_cursor.fetchone()
+
+        location = Location(**result)
+
+        return json.dumps(location.__dict__)
 
 def create_location(location):
     location["id"] = get_next_id(LOCATIONS)
